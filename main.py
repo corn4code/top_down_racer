7b7cc656e5853f73ce1204f9de2cf73237ed3f1a
corn4code <cornelius_weigert@outlook.de>
2021-06-02 08:45:47 +0200

    idk

--- main.py
+++ main.py
@@ -21,36 +21,40 @@ clock = pygame.time.Clock()
 bg_surface = pygame.transform.scale2x(pygame.image.load("bg.png").convert())
 running = True
 while running:
-    speed_x += acceleration_x
-    speed_y += acceleration_y
-    if speed_x > 3:
-        speed_x = 3
-    if speed_x < -3:
-        speed_x = -3
-    if speed_y > 3:
-        speed_y = 3
-    if speed_y < -3:
-        speed_y = -3
-    car_x += speed_x
-    car_y += speed_y
+    # speed_x += acceleration_x
+    # speed_y += acceleration_y
+    # if speed_x > 3:
+    #     speed_x = 3
+    # if speed_x < -3:
+    #     speed_x = -3
+    # if speed_y > 3:
+    #     speed_y = 3
+    # if speed_y < -3:
+    #     speed_y = -3
+    # car_x += speed_x
+    # car_y += speed_y
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                acceleration_y = -0.1
-            if event.key == pygame.K_DOWN:
-                acceleration_y = 0.1
-            if event.key == pygame.K_RIGHT:
-                acceleration_x = 0.1
-            if event.key == pygame.K_LEFT:
-                acceleration_x = -0.1
-            if event.key == pygame.K_SPACE:
-                speed_y = 0
-                speed_x = 0
-                acceleration_y = 0
-                acceleration_x = 0
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                # acceleration_y = -0.1
+                car_y -= 10
+            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                # acceleration_y = 0.1
+                car_y += 10
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                # acceleration_x = 0.1
+                car_x += 10
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                # acceleration_x = -0.1
+                car_x -= 10
+            # if event.key == pygame.K_SPACE:
+                # speed_y = 0
+                # speed_x = 0
+                # acceleration_y = 0
+                # acceleration_x = 0
 
     screen.blit(bg_surface, (0, 0))
 
