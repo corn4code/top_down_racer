6b71df21d2ada6fb79ec13feb87b549d46694850
corn4code <cornelius_weigert@outlook.de>
2021-08-30 21:20:03 +0200

    changed track, added car image, adjusted movement

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import pygame
 # like wheel for movement in mobile games
 
 def draw_car():
-    pygame.draw.rect(screen, (0, 0, 255), (car_x, car_y, 90, 50))
+    pygame.draw.rect(screen, (0, 0, 255), (car_x, car_y, 50, 50))
+    rect = car.get_rect()
+    rect.topleft = (car_x, car_y)
+    screen.blit(car, rect)
 
 def draw_rotation_car():
     
@@ -18,33 +21,36 @@ window_height = 1030
 size = [window_width, window_height]
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-bg_surface = pygame.transform.scale2x(pygame.image.load("bg.png").convert())
+bg_surface = pygame.image.load("track1.png").convert()
+car = pygame.image.load("car_vers1.png").convert()
 running = True
+
 while running:
 
     # start movement of car
     keys = pygame.key.get_pressed()  #checking pressed keys
     if keys[pygame.K_UP] and keys[pygame.K_RIGHT]:
-        car_x += 1.5
-        car_y -= 1.5
+        car_x += 1.25
+        car_y -= 1.25
         draw_rotation_car()
     elif keys[pygame.K_UP] and keys[pygame.K_LEFT]:
-        car_x -= 1.5
-        car_y -= 1.5
+        car_x -= 1.25
+        car_y -= 1.25
     elif keys[pygame.K_DOWN] and keys[pygame.K_RIGHT]:
-        car_x += 1.5
-        car_y += 1.5
+        car_x += 1.25
+        car_y += 1.25
     elif keys[pygame.K_DOWN] and keys[pygame.K_LEFT]:
-        car_x -= 1.5
-        car_y += 1.5
+        car_x -= 1.25
+        car_y += 1.25
     elif keys[pygame.K_UP]:
-        car_y -= 3
+        car_y -= 2.5
     elif keys[pygame.K_DOWN]:
-        car_y += 3
+        car_y += 2.5
     elif keys[pygame.K_RIGHT]:
-        car_x += 3
+        car_x += 2.5
     elif keys[pygame.K_LEFT]:
-        car_x -= 3
+        car_x -= 2.5
+        
     # end movement of car
     
     
